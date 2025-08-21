3cd2daf3c9183c642d771ec843380f2f7c0ef522
Vips43 <vipulgehlot43@gmail.com>
2025-08-21 23:22:45 +0530

    de

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.querySelector('input'),
 btn = document.querySelector('.btn'),
 main = document.querySelector('.main'),
+reload = document.querySelector('.reload'),
 qrImg = document.querySelector('.qr-img img')
 console.log(input,btn);
 
@@ -9,16 +10,21 @@ btn.onclick = (e) =>{
     if (qrValue == '') {
         return
     }
-    qrImg.classList.add("active")
-    main.classList.add("active")
     console.log(qrValue);
     e.target.textContent = 'Generating Qr code...'
+    
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
+    
+    qrImg.addEventListener('load',()=>{
+        qrImg.classList.add("active")
+        main.classList.add("active")
+        e.target.textContent = 'Generate Qr code'
+        btn.disabled = true
+    })    
+    reload.addEventListener('click',()=>{
+        location.reload()
+    })
 
-    async function getQrData() {
-        const getData = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`)
-
-        qrImg.src = getData
-    }
 }
 
 
